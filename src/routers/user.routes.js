d007b791e01c4c92be06ad6321124864566ba9a0
Julio <juliolk.4321@gmail.com>
2022-12-13 11:59:08 -0300

    criando rotas e controllers de usuario 1/2

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,8 +1,10 @@
 import Express from "express";
+import createUser from "../controllers/user/createUser.js";
+import getUsers from "../controllers/user/getUsers.js";
 const userRoute = Express.Router();
 
-userRoute.get("/user");
-userRoute.post("/user"); // vírgula, a função que irá vim do controller de criar usuario
+userRoute.get("/user", getUsers);
+userRoute.post("/user", createUser); // vírgula, a função que irá vim do controller de criar usuario
 userRoute.put("/user/:id");
 userRoute.delete("/user/:id");
 userRoute.post("/login");
