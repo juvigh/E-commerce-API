f91f6d940073b3064beb592515253ba947525198
Julio <juliolk.4321@gmail.com>
2022-12-17 16:43:44 -0300

    Implementando crud de carrinho

---
+++ src/models/Cart.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
+class Cart {
+  constructor(user_id, product_id, status) {
+    (this.id = uuidv4()),
+      (this.user_id = user_id),
+      (this.product_id = product_id),
+      (this.status = status);
+  }
+}
+
+export default Cart;
