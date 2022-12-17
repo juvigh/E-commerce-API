385133d4b1122da56912251597e58cb61783e518
Julio <juliolk.4321@gmail.com>
2022-12-17 16:40:35 -0300

    Implementando crud usuario

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,32 @@
-import { v4 as uuidv4 } from 'uuid';
-import { sha256 } from 'js-sha256';
-
-class User {
-    constructor(name, email, password) {
-        this.id = uuidv4(),
-        this.name = name,
-        this.email = email,
-        this.password = sha256(password)
+import { v4 as uuidv4 } from "uuid";
+import { sha256 } from "js-sha256";
+export class User {
+  validPassword(password) {
+    if (password) {
+      return sha256(password);
+    } else {
+      return "";
     }
+  }
+
+  constructor(name, email, password) {
+    (this.id = uuidv4()),
+      (this.name = name),
+      (this.email = email),
+      (this.password = this.validPassword(password));
+  }
 }
 
-export default User;
+export class UserAuth {
+  validPassword(password) {
+    if (password) {
+      return sha256(password);
+    } else {
+      return "";
+    }
+  }
+  constructor(email, password) {
+    (this.email = email), (this.password = this.validPassword(password));
+  }
+}
+
